7bf31d06acfbb4d741c167c1023e0a147c7ffc73
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-05-16 17:47:43 -0600

    hash_pass.py

--- hash_pass.py
+++ hash_pass.py
@@ -22,7 +22,9 @@ def input_password_user():
    f=sha512("sha512".encode('utf8')).hexdigest()
 
 
-
+   print("""CONSEJO:usar una contraseña mayor de 8 caracteres y que tenga números,símbolos,Mayusculas y minusculas""")
+   sleep(3)
+   system("clear")
    password_user=input("ingresa la contrasena de tu login: ")
    sleep(1)
    system("clear")
