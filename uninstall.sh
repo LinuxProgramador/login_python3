7bf22ad90a1c38111ca716ff915d0f4702504046
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-09-03 09:59:03 -0600

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,44 +2,28 @@
 
 
 function uninstall_login_termux {
-
  trap ctrl_c 2
-
  function ctrl_c() {
 
        echo $(clear)
        echo "Bye"
        exit
  }
-
-
-
  login="$PATH/login.py"
-
  if [ -f "$login" ] ; then
-
      rm -f $PATH/login.py
      echo "el archivo login.py a sido eliminado con exito de la ruta: $PATH"
      sleep 5
      echo $(clear)
-
  else
      echo "no existe el archivo login.py en la ruta: $PATH"
      sleep 5
      echo $(clear)
-
  fi
-
-
-
-
-
  mv ~/login_python3/motd  /data/data/com.termux/files/usr/etc/motd
  echo "se restablecio el archivo motd a su nombre por defecto en la ruta: $PREFIX/etc"
  sleep 5
  echo $(clear)
-
-
  cd ~
  rm -rf ~/login_python3
  cd ~
@@ -51,35 +35,22 @@ function uninstall_login_termux {
  echo "cd $PREFIX/etc"
  echo "nano bash.bashrc"
  echo "borra en donde dice: python3 PATH/login.py"
-
 }
 
-
-
-
-
 function main {
-
  trap ctrl_c 2
-
  function ctrl_c() {
 
        echo $(clear)
        echo "Bye"
        exit
  }
-
-
  read -p "desea eliminar login termux (si/no): " delete_login_termux
  if [ "$delete_login_termux" = 'si' ] ; then
-
        secure_token=$(python3 ~/login_python3/.hash_uninstall.py)
        secure_token_input=$(cat ~/login_python3/.password_hash_uninstall.txt | cut -f1)
-
        if [ "$secure_token" = "$secure_token_input"  ] ; then
                uninstall_login_termux
-
-
        else
              echo "password invalida"
              exit
@@ -87,7 +58,5 @@ function main {
  else
       echo "operacion cancelada"
  fi
-
-
 }
 main
