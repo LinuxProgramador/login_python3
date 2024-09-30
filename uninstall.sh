a012b9aabc1540ad097d13a0863af7c4a4789f07
LinuxProgramador <154765747+LinuxProgramador@users.noreply.github.com>
2024-09-29 19:54:43 -0600

    uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,29 +5,29 @@ login="$PATH/login.py"
 function uninstall_login_termux {
  if [ -f "$login" ] ; then
      rm -f $PATH/login.py
-     echo "el archivo login.py a sido eliminado con exito de la ruta: $PATH"
+     echo "El archivo login.py a sido eliminado con exito de la ruta: $PATH"
      sleep 5
      echo $(clear)
  else
-     echo "no existe el archivo login.py en la ruta: $PATH"
+     echo "No existe el archivo login.py en la ruta: $PATH"
      sleep 5
      echo $(clear)
  fi
  mv ~/login_python3/motd  /data/data/com.termux/files/usr/etc/motd
- echo "se restablecio el archivo motd a su nombre por defecto en la ruta: $PREFIX/etc"
+ echo "Se restablecio el archivo motd a su nombre por defecto en la ruta: $PREFIX/etc"
  sleep 5
  echo $(clear)
  cd ~
  rm -rf ~/login_python3
  cd ~
- echo "se borro el directorio login_python3 del directorio HOME"
+ echo "Se borro el directorio login_python3 del directorio HOME"
  echo "NOTA:si configuraste el bash.bashrc para cargar el script login termux"
  echo ""
  echo "Tienes que seguir estos pasos para desactivarlo"
  echo ""
  echo "cd $PREFIX/etc"
  echo "nano bash.bashrc"
- echo "borra en donde dice: python3 PATH/login.py"
+ echo "Borra en donde dice: python3 PATH/login.py"
 }
 
 function main {
